3bf189abed8aaf5c7e91b1f4b91237cec593c0e1
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2024-07-21 12:54:40 +0530

    Add lucia auth

---
+++ src/lib/lucia.ts
@@ -0,0 +1,40 @@
+import { Lucia } from 'lucia'
+import { PrismaAdapter } from '@lucia-auth/adapter-prisma'
+
+import prisma from './prisma'
+
+const adapter = new PrismaAdapter(prisma.session, prisma.user)
+
+export const lucia = new Lucia(adapter, {
+  sessionCookie: {
+    expires: false,
+    attributes: {
+      secure: process.env.NODE_ENV === 'production',
+    },
+  },
+  getUserAttributes: (attributes) => {
+    return {
+      // attributes has the type of DatabaseUserAttributes
+      id: attributes.id,
+      username: attributes.username,
+      displayName: attributes.displayName,
+      avatarUrl: attributes.avatarUrl,
+      googleId: attributes.googleId,
+    }
+  },
+})
+
+declare module 'lucia' {
+  interface Register {
+    Lucia: typeof lucia
+    DatabaseUserAttributes: DatabaseUserAttributes
+  }
+}
+
+interface DatabaseUserAttributes {
+  id: string
+  username: string
+  displayName: string
+  avatarUrl: string | null
+  googleId: string | null
+}
