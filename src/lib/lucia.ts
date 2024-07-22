3b911a4b949078a18de723348079ef599af75ba6
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2024-07-22 19:55:17 +0530

    Add validate request fn

--- src/lib/lucia.ts
+++ src/lib/lucia.ts
@@ -1,4 +1,6 @@
-import { Lucia } from 'lucia'
+import { cache } from 'react'
+import { cookies } from 'next/headers'
+import { Lucia, User, Session } from 'lucia'
 import { PrismaAdapter } from '@lucia-auth/adapter-prisma'
 
 import prisma from './prisma'
@@ -38,3 +40,42 @@ interface DatabaseUserAttributes {
   avatarUrl: string | null
   googleId: string | null
 }
+
+type AuthPayload =
+  | { user: User; session: Session }
+  | { user: null; session: null }
+
+export const validateRequest = cache(async (): Promise<AuthPayload> => {
+  const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null
+
+  if (!sessionId) {
+    return {
+      user: null,
+      session: null,
+    }
+  }
+
+  const result = await lucia.validateSession(sessionId)
+
+  try {
+    if (result.session && result.session.fresh) {
+      const sessionCookie = lucia.createSessionCookie(result.session.id)
+      cookies().set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes
+      )
+    }
+
+    if (!result.session) {
+      const sessionCookie = lucia.createBlankSessionCookie()
+      cookies().set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes
+      )
+    }
+  } catch {}
+
+  return result
+})
